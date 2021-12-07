62cc27e32e3feb683508e94661cac2c03980cbd5
Kevin Blues <kbluescode@gmail.com>
2021-12-06 22:57:00 -0800

    better refactors

--- aoc-day03/src/main.rs
+++ aoc-day03/src/main.rs
@@ -1,30 +1,26 @@
 // https://adventofcode.com/2021/day/3
 
-const ONE: char = '1';
-const ZERO: char = '0';
-
-fn parse_lines(text: &str) -> Vec<&str> {
-    text.split('\n').collect()
+fn parse_lines(text: &str) -> (Vec<i32>, u8) {
+    let nums = text
+        .lines()
+        .map(|line| i32::from_str_radix(line, 2).unwrap())
+        .collect();
+    (nums, text.lines().next().unwrap().len() as u8)
 }
 
-fn gamma_epsilon(lines: &[&str]) -> (i32, i32) {
-    let mut stack = Vec::new();
+fn gamma_epsilon(nums: &[i32], power: u8) -> (i32, i32) {
     let mut gamma = String::new();
     let mut epsilon = String::new();
-
-    for i in 0..lines[0].len() {
-        for line in lines {
-            stack.push(line.chars().nth(i).unwrap());
-        }
-        let (ones, zeroes): (Vec<char>, Vec<char>) = stack.iter().partition(|num| **num == ONE);
+    for i in (0..power).rev() {
+        let check = (1 << i) as i32;
+        let (ones, zeroes): (Vec<i32>, Vec<i32>) = nums.iter().partition(|&num| num & check != 0);
         if ones.len() > zeroes.len() {
-            gamma.push(ONE);
-            epsilon.push(ZERO);
+            gamma.push('1');
+            epsilon.push('0');
         } else {
-            gamma.push(ZERO);
-            epsilon.push(ONE);
+            gamma.push('0');
+            epsilon.push('1');
         }
-        stack.clear();
     }
 
     let gamma = i32::from_str_radix(&gamma, 2).unwrap();
@@ -34,35 +30,44 @@ fn gamma_epsilon(lines: &[&str]) -> (i32, i32) {
 
 use std::cmp::Ordering;
 
-fn find_match(lines: &[&str], positive: char, negative: char) -> i32 {
-    let mut matches: Vec<&str> = lines.iter().copied().collect();
+fn oxygen_rating(nums: &[i32], power: u8) -> i32 {
+    let mut numbers = nums.to_vec();
 
-    for idx in 0..lines[0].len() {
-        let match_iter = matches.iter();
-        if match_iter.len() == 1 {
+    for i in (0..power).rev() {
+        let check = (1 << i) as i32;
+        let num_iter = numbers.iter();
+        if num_iter.len() <= 1 {
             break;
         }
-        let (ones, zeroes): (Vec<&str>, Vec<&str>) =
-            match_iter.partition(|&&line| line.chars().nth(idx).unwrap() == ONE);
+        let (ones, zeroes): (Vec<i32>, Vec<i32>) = num_iter.partition(|&num| num & check != 0);
 
-        let matcher = match ones.len().cmp(&zeroes.len()) {
-            Ordering::Greater => positive,
-            Ordering::Less => negative,
-            Ordering::Equal => positive,
-        };
-
-        matches.retain(|&line| line.chars().nth(idx).unwrap() == matcher);
+        match ones.len().cmp(&zeroes.len()) {
+            Ordering::Greater | Ordering::Equal => numbers = ones,
+            Ordering::Less => numbers = zeroes,
+        }
     }
 
-    i32::from_str_radix(matches.last().unwrap(), 2).unwrap()
+    *numbers.last().unwrap()
 }
 
-fn oxygen_rating(lines: &[&str]) -> i32 {
-    find_match(lines, ONE, ZERO)
-}
+fn co2_rating(nums: &[i32], power: u8) -> i32 {
+    let mut numbers = nums.to_vec();
 
-fn co2_rating(lines: &[&str]) -> i32 {
-    find_match(lines, ZERO, ONE)
+    for i in (0..power).rev() {
+        let check = (1 << i) as i32;
+        let num_iter = numbers.iter();
+        if num_iter.len() <= 1 {
+            break;
+        }
+        let (ones, zeroes): (Vec<i32>, Vec<i32>) = num_iter.partition(|&num| num & check != 0);
+
+        match ones.len().cmp(&zeroes.len()) {
+            Ordering::Greater | Ordering::Equal => numbers = zeroes,
+            Ordering::Less => numbers = ones,
+        }
+    }
+
+    *numbers.last().unwrap()
 }
 
 #[cfg(test)]
@@ -71,32 +76,32 @@ mod test {
 
     #[test]
     fn gamma_epsilon_valid() {
-        let lines = parse_lines(include_str!("../test_input.txt"));
+        let (lines, power) = parse_lines(include_str!("../test_input.txt"));
         let expected = (22, 9);
-        assert_eq!(expected, gamma_epsilon(&lines));
+        assert_eq!(expected, gamma_epsilon(&lines, power));
     }
 
     #[test]
     fn oxygen_rating_valid() {
-        let lines = parse_lines(include_str!("../test_input.txt"));
+        let (lines, power) = parse_lines(include_str!("../test_input.txt"));
         let expected = 23;
-        assert_eq!(expected, oxygen_rating(&lines));
+        assert_eq!(expected, oxygen_rating(&lines, power));
     }
 
     #[test]
     fn co2_rating_valid() {
-        let lines = parse_lines(include_str!("../test_input.txt"));
+        let (lines, power) = parse_lines(include_str!("../test_input.txt"));
         let expected = 10;
-        assert_eq!(expected, co2_rating(&lines));
+        assert_eq!(expected, co2_rating(&lines, power));
     }
 }
 
 fn main() {
-    let lines = parse_lines(include_str!("../input.txt"));
-    let (gamma, epsilon) = gamma_epsilon(&lines);
+    let (lines, power) = parse_lines(include_str!("../input.txt"));
+    let (gamma, epsilon) = gamma_epsilon(&lines, power);
     println!("Part 1 Result: {}", gamma * epsilon);
 
-    let oxygen = oxygen_rating(&lines);
-    let co2 = co2_rating(&lines);
+    let oxygen = oxygen_rating(&lines, power);
+    let co2 = co2_rating(&lines, power);
     println!("Part 2 Result: {}", oxygen * co2);
 }
